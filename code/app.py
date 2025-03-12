1190a3b967a095a74049861c6b4d0a9e2723592c
bnguyen700 <brandy.nguyen02@gmail.com>
2025-03-12 12:18:18 -0700

    Add project skeleton functions
    *Created the function required for the project
    
    *Added secret key for sessions

--- code/app.py
+++ code/app.py
@@ -1,11 +1,49 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
 
-@app.route('/')
+# In memory database 
+userLogin = [
+    {"test1" : 123},
+    {"test2" : 4321}
+]
 
-def hello_world():
-    return 'Hello, World!'
+users = {}
+
+# User login
+@app.route('/login', METHOD=['POST'])
+def login():
+    return 'Login'
+
+# User registration 
+@app.route('/register', METHOD=['POST'])
+def register():
+    return 'Register'
+
+# User logout
+@app.route('/logout', METHOD=['POST'])
+def logout():
+    return 'Logout'
+
+# Inventory management (CRUD)
+@app.route('/createInvetoryItem', METHOD=['POST'])
+def create_inventory_item():
+    return 'Item created'
+
+@app.route('/readInventoryItem', METHOD=['GET'])
+def read_inventory_item():
+    return 'Item read'
+
+@app.route('/updateInventoryItem', METHOD=['PUT'])
+def update_inventory_item():
+    return 'Item updated'
+
+@app.route('/deleteInventoryItem', METHOD=['DELETE'])
+def delete_inventory_item():
+    return 'Item deleted'
+
+# Cookies and session management
 
 if __name__ == '__main__':
     app.run()
